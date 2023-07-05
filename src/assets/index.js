1f494350bb0212177a344e3cdf4284803c89f11c
Pauline Schmiechen <generalpaulinski@users.noreply.github.com>
2023-07-05 15:13:07 +0200

    Add design file

---
+++ src/assets/index.js
@@ -0,0 +1,71 @@
+import frame1 from "./frame1.png";
+import frame2 from "./frame2.png";
+import frame3 from "./frame3.png";
+import frame4 from "./frame4.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import axel from "./company/axel.jpg";
+import delivery from "./company/delivery.png";
+import voiyal from "./company/voiyal.png";
+import audienz from "./company/audienz.app.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+import font from "./fonts/helvetiker_bold.typeface.json";
+import videoSrc from "./drei.mp4";
+import grass from './line-of-grass.png';
+import unicorn from './giphy.gif';
+import dolphin from './dolphin.gif';
+
+export {
+  frame1,
+  frame2,
+  frame3,
+  frame4,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  axel,
+  delivery,
+  voiyal,
+  audienz,
+  carrent,
+  jobit,
+  tripguide,
+  font,
+  videoSrc,
+  grass,
+  unicorn,
+  dolphin
+};
